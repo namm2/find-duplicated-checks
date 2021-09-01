a2e26c63f0472e025b4a5fbce5a8516f078105b9
Nam Hai Nguyen <namhai.nguyen-ext@commercetools.com>
2021-09-01 06:06:23 +0200

    Correct dup list

--- main.go
+++ main.go
@@ -25,21 +25,19 @@ func main() {
 	log.Println(len(checksList), "Total checks")
 
 	unique := make([]string, 0)
-	dupItem := make(map[string]int)
-	dupList := make([]map[string]int, 0)
+	dup := make(map[string]int)
 check:
 	for _, c := range checksList {
 		for _, j := range unique {
 			if c.Name == j {
-				dupItem[c.Name] += 1
-				dupList = append(dupList, dupItem)
+				dup[c.Name] += 1
 				continue check
 			}
 		}
 		unique = append(unique, c.Name)
 	}
-	if len(dupList) > 0 {
-		log.Println("Found duplicated checks: ", dupList)
+	if len(dup) > 0 {
+		log.Println("Found duplicated checks: ", dup)
 	}
 	log.Println(len(unique), "Unique checks")
 }
